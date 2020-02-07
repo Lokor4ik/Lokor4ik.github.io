faf66ce8cf03b3e80514653ed230bd8b51ff9e9c
Lokor4ick <lokoryshka@users.noreply.github.com>
2020-02-07 16:57:30 +0200

    init commit

---
+++ dist/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('.slider__items').slick({
+        slidesToShow: 3,
+        prevArrow: '<div class="prev"></div>',
+        nextArrow: '<div class="next"></div>',
+    });
+});
