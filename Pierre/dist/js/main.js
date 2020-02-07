8a82eaed45cdb02c97ae466d6b1316d819166d55
Lokor4ick <lokoryshka@users.noreply.github.com>
2020-02-08 01:49:04 +0200

    init commit

---
+++ Pierre/dist/js/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    $('.slider__items').slick({
+        slidesToShow: 3,
+        prevArrow: '<div class="prev"></div>',
+        nextArrow: '<div class="next"></div>',
+        responsive: [{
+                breakpoint: 950,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 647,
+                settings: {
+                    slidesToShow: 1,
+                    dots: true,
+                    nextArrow: false,
+                    prevArrow: false
+                }
+            }
+        ]
+    });
+});
+var wrapperMenu = document.querySelector('.wrapper-menu'); //BURGER
+
+wrapperMenu.addEventListener('click', function() {
+    wrapperMenu.classList.toggle('open');
+})
+$('.wrapper-menu').on('click',
+    function() {
+        $('.menu ul').slideToggle();
+    });
